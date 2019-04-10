7d06399bd9c95ff8782a4485f6daace8380c1085
Nydrani <victorzhang6897@gmail.com>
2019-04-10 14:01:18 +1000

    Added parsing into database

--- markethistoryparser.py
+++ markethistoryparser.py
@@ -1,39 +1,33 @@
 #!/usr/local/bin/python3
 
+import peewee
 import pprint
 import json
 import re
 
-class Item:
-    def __init__(self, name, amount, cost):
-        self.name = name
-        self.amount = amount
-        self.cost = cost
+db = peewee.SqliteDatabase('market.db')
 
-    def __str__(self):
-        return self.name + " | " + str(self.amount) + " | " + str(self.cost) + " mesos"
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
 
-    def __repr__(self):
-        return str(self)
+class Item(BaseModel):
+    name = peewee.CharField(unique=True, max_length=64)
 
-class Equip(Item):
-    def __init__(self, name, amount, cost, stats, slots):
-        super().__init__(name, amount, cost)
-        self.stats = stats
-        self.slots = slots
+class Sale(BaseModel):
+    item_id = peewee.ForeignKeyField(Item)
+    amount = peewee.SmallIntegerField()
+    cost = peewee.IntegerField()
+    time = peewee.DateTimeField()
 
-    def __str__(self):
-        return super().__str__() + " | " + str(self.stats) + " | " + str(self.slots)
+db.connect()
 
-    def __repr__(self):
-        return str(self)
 
 def parseEquipContents(contents):
     slotRegex1 = re.compile('\((.* slots)\)')
     slotRegex2 = re.compile('\((\+\d+\/\d+)\)')
     statsRegex = re.compile('\(((?:[A-Z.]+ \+\d+ )+)\)')
     s = []
-    regexList = [slotRegex1, slotRegex2, statsRegex]
 
     # split all matching (parenthises) pairs
     for e in contents.split('('):
@@ -69,84 +63,98 @@ def parseEquipContents(contents):
             continue
 
     # merge everything else into the name
+    # equips are not stackable, so amount would not be merged into the name
     name = ' '.join(s[:])
 
     return name, stats, slots 
 
 
-itemList = []
+def loadDatabase():
+    db.drop_tables([Item, Sale])
+    db.create_tables([Item, Sale])
 
-with open('market_history.log', 'r') as f:
-    for line in f:
-        s = json.loads(line)
-
-        if s['type'] != 'sold':
-            print("ignore listing types")
-            print(s)
-            continue
-
-        style1 = re.compile('\( *Channel +\d +FM +\d *\) \*\*I just sold a\(n\) (.*) to .* for (.*) mesos!\*\*')
-        style2 = re.compile('\( *Channel +\d +FM +\d *\) \*\*I just sold a (.*) to .* for (.*) mesos!\*\*')
-        style3 = re.compile('\( *Channel +\d +FM +\d *\) \*\*I just sold a (.*) for (.*) mesos!\*\*')
-        regexList = [style1, style2, style3]
-
-        for style in regexList:
-            matched = style.match(s['content'])
-
-            # found nothing --> try another
-            if matched:
-                break
-
-        if matched is None:
-            print("problem here (strange input format)")
-            print(s)
-            continue
-
-        # split item and cost
-        item = matched.group(1)
-        cost = matched.group(2).replace(',', '')
-        amount = 1
-        name = None
-        stats = None
-        slots = None
-
-        # code to item bunches
-        bunchRegex = re.compile('(.*) \((\d+)\)$')
-        m = bunchRegex.match(item)
-        if m:
-            # edge case for icarus cape
-            if m.group(1) == 'Icarus Cape':
-                pass
-            else:
-                # item was sold in a bunch
-                amount = m.group(2)
-                item = m.group(1)
+    with open('market_history.log', 'r') as f:
+        for line in f:
+            s = json.loads(line)
     
-        # code to match equips
-        equipRegex = re.compile('.*\(.*\)')
-        if equipRegex.match(item):
-            # item is equip
-            # parse equip
-            #print("Weapon: " + item + " | " + cost)
-            name, stats, slots = parseEquipContents(item)
-            equip = Equip(name, amount, cost, stats, slots)
+            if s['type'] != 'sold':
+                print("ignore listing types")
+                print(s)
+                continue
+    
+            style1 = re.compile('\( *Channel +\d +FM +\d *\) \*\*I just sold a\(n\) (.*) to .* for (.*) mesos!\*\*')
+            style2 = re.compile('\( *Channel +\d +FM +\d *\) \*\*I just sold a (.*) to .* for (.*) mesos!\*\*')
+            style3 = re.compile('\( *Channel +\d +FM +\d *\) \*\*I just sold a (.*) for (.*) mesos!\*\*')
+            regexList = [style1, style2, style3]
+    
+            for style in regexList:
+                matched = style.match(s['content'])
+    
+                # found nothing --> try another
+                if matched:
+                    break
+    
+            if matched is None:
+                print("problem here (strange input format)")
+                print(s)
+                continue
+    
+            # split item and cost
+            item = matched.group(1)
+            cost = matched.group(2).replace(',', '')
+            amount = 1
+            date = s['created_at']
+            name = None
+            stats = None
+            slots = None
+    
+            # code to item bunches
+            bunchRegex = re.compile('(.*) \((\d+)\)$')
+            m = bunchRegex.match(item)
+            if m:
+                # edge case for icarus cape
+                if m.group(1) == 'Icarus Cape':
+                    pass
+                else:
+                    # item was sold in a bunch
+                    amount = m.group(2)
+                    item = m.group(1)
+        
+            name = item
+    
+            # code to match equips
+            # for now ignore equips (since they fluctuate based on price and slots)
+            equipRegex = re.compile('.*\(.*\)')
+            if equipRegex.match(item):
+                # item is equip
+                # print("Weapon: " + item + " | " + cost)
+                # parse equip
+                name, stats, slots = parseEquipContents(item)
+                continue
+    
+            # code to match scrolls (useless for now)
+            scrollRegex = re.compile('^Scroll for .*')
+            if scrollRegex.match(item):
+                # item is scroll
+                # print("Scroll: " + item + " | " + cost)
+                pass
+    
+            # load into db
+            try:
+                Item.create(name=name)
+            except peewee.IntegrityError as e:
+                if "UNIQUE" not in str(e):
+                    print(e)
+    
+            # load sale into db
+            try:
+                obj = Item.get(Item.name == name)
+                print("found " + name)
+                Sale.create(item_id=obj, amount=amount, cost=cost, date=date)
+            except Item.DoesNotExist as e:
+                print(e)
+    
 
-            itemList.append(equip)
-            continue
-
-        # code to match scrolls (useless for now)
-        scrollRegex = re.compile('Scroll for .*')
-        if scrollRegex.match(item):
-            # item is scroll
-            #print("Scroll: " + item + " | " + cost)
-            obj = Item(item, amount, cost)
-            itemList.append(obj)
-            continue
-
-        # others
-        obj = Item(item, amount, cost)
-        itemList.append(obj)
-
-    # print everything
-    pprint.pprint(itemList)
-
+loadDatabase()
+for i in Sale.select().join(Item, peewee.JOIN.LEFT_OUTER):
+    print(i.time)
